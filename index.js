f22d4e59ba556173eaf1d1ec2e22a23480d3f284
BARATH KUMAR S <50653477+barath99@users.noreply.github.com>
2019-08-04 08:47:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,321 @@
+// var questions = []; var answers = [];
+// questions[0]="What are the grass lands of South America called?";
+// answers[0]="Stepes";
+// answers[1]="Prairies";
+// answers[2]="Pampas";
+// answers[3]="Savanna";
+// answers[80]="Pampas";
+//
+// questions[1]="Which of the followings causes Earthquakes?";
+// answers[4]="Volcanic eruptions";
+// answers[5]="Landslides";
+// answers[6]="Cyclones";
+// answers[7]="Movement of a part of earth’s surface on account of the faulting of rocks";
+// answers[82]="Movement of a part of earth’s surface on account of the faulting of rocks";
+//
+// answers[81]="Pampas";
+//
+// questions[2]="What are the grass lands of South America called?";
+//
+// answers[81]="Pampas";
+//
+// questions[3]="What are the grass lands of South America called?";
+//
+// answers[81]="Pampas";
+//
+// questions[4]="What are the grass lands of South America called?";
+// answers[5]="Stepes";
+// answers[6]="Prairies";
+// answers[7]="Pampas";
+// answers[8]="Savanna";
+// answers[81]="Pampas";
+//
+// questions[5]="What are the grass lands of South America called?";
+// answers[5]="Stepes";
+// answers[6]="Prairies";
+// answers[7]="Pampas";
+// answers[8]="Savanna";
+// answers[81]="Pampas";
+//
+// questions[6]="What are the grass lands of South America called?";
+// answers[5]="Stepes";
+// answers[6]="Prairies";
+// answers[7]="Pampas";
+// answers[8]="Savanna";
+// answers[81]="Pampas";
+//
+// questions[7]="What are the grass lands of South America called?";
+// answers[5]="Stepes";
+// answers[6]="Prairies";
+// answers[7]="Pampas";
+// answers[8]="Savanna";
+// answers[81]="Pampas";
+//
+// questions[8]="What are the grass lands of South America called?";
+// questions[9]="What are the grass lands of South America called?";
+// questions[10]="What are the grass lands of South America called?";
+// questions[12]="What are the grass lands of South America called?";
+// questions[11]="What are the grass lands of South America called?";
+// questions[13]="What are the grass lands of South America called?";
+// questions[14]="What are the grass lands of South America called?";
+// questions[15]="What are the grass lands of South America called?";
+// questions[16]="What are the grass lands of South America called?";
+// questions[17]="What are the grass lands of South America called?";
+// questions[18]="What are the grass lands of South America called?";
+var myObj={
+  "q1": {
+                "question": "What are the grass lands of South America called?",
+                "options": [
+                    "Stepes",
+                    "Prairies",
+                    "Pampas",
+                    "Savanna"
+                ],
+                "answer": "Pampas"
+      },
+  "q2": {
+                "question": "Which of the followings causes Earthquakes?",
+                "options": [
+                    "Volcanic eruptions",
+                    "Landslides",
+                    "Cyclones",
+                    "Movement of a part of earth’s surface on account of the faulting of rocks"
+                ],
+                "answer": "Movement of a part of earth’s surface on account of the faulting of rocks"
+      },
+"q3": {
+                "question": "Which of the followings is the reason behind the different seasons to occur?",
+                "options": [
+                    "Sun is moving around the earth",
+                    "Of revolution of the earth around the sun on its orbit",
+                    "Tilt of Earth’s axis",
+                    "All of the above"
+                ],
+                "answer": "Of revolution of the earth around the sun on its orbit"
+      },
+"q4": {
+                "question": "Which one is correct team name in NBA?",
+                "options": [
+                    "New York Bulls",
+                    "Los Angeles Kings",
+                    "Golden State Warriros",
+                    "Huston Rocket"
+                ],
+                "answer": "Huston Rocket"
+      },
+"q5": {
+                "question": "How does forests help soil?",
+                "options": [
+                    "Soil erosion",
+                    "Soil protection",
+                    "Depleting the soil of its moisture",
+                    "All of the above"
+                ],
+                "answer": "Soil protection"
+      },
+"q6": {
+                "question": "The varieties of corn can be improved by which of the following methods?",
+                "options": [
+                    "Dihybrid crosses",
+                    "Back cross",
+                    "Double cross",
+                    "Natural selection"
+                ],
+                "answer": "Double cross"
+      },
+"q7": {
+                "question": "Which soil is best suited for paddy crop?",
+                "options": [
+                    "Black soil",
+                    "Loamy soil",
+                    "Hard soil",
+                    "Red soil"
+                ],
+                "answer": "Loamy soil"
+      },
+"q8": {
+                "question": "Which of the following statements about the cultivation of tea and coffee is true?",
+                "options": [
+                    "Tea grows at a higher altitude than coffee",
+                    "Coffee grows at a higher altitude than tea",
+                    "Both grow at about the same altitude",
+                    "There is no climatic relationship between the two crops"
+                ],
+                "answer": "Tea grows at a higher altitude than coffee"
+      },
+"q9": {
+                "question": "Which of the following crops has the highest photosynthetic activity?",
+                "options": [
+                    "Cotton",
+                    "Sugarcane",
+                    "Rice",
+                    "Wheat"
+                ],
+                "answer": "Sugarcane"
+      },
+"q10": {
+  "question": "How does the Hybridisation develop improved crops?",
+  "options": [
+      "Creating new and better genes",
+      "Eliminating undesirable genes",
+      "Producing favourable combination of genes",
+      "By encouraging favourable genes and removing unfavourable ones"
+  ],
+  "answer": "Producing favourable combination of genes"
+      },
+"q11": {
+  "question": "Which of the following is the most common wheat disease in India?",
+  "options": [
+      "Green ear",
+      "Smut",
+      "Loose smut",
+      "Rust"
+  ],
+  "answer": "Rust"
+      },
+// "q12": {
+//   "question": "Which of the following crops has the highest photosynthetic activity?",
+//   "options": [
+//       "Cotton",
+//       "Sugarcane",
+//       "Rice",
+//       "Wheat"
+//   ],
+//   "answer": "Sugarcane"
+//       },
+// "q13": {
+//   "question": "Which of the following crops has the highest photosynthetic activity?",
+//   "options": [
+//       "Cotton",
+//       "Sugarcane",
+//       "Rice",
+//       "Wheat"
+//   ],
+//   "answer": "Sugarcane"
+//       },
+// "q14": {
+//   "question": "Which of the following crops has the highest photosynthetic activity?",
+//   "options": [
+//       "Cotton",
+//       "Sugarcane",
+//       "Rice",
+//       "Wheat"
+//   ],
+//   "answer": "Sugarcane"
+//       },
+// "q15": {
+//   "question": "Which of the following crops has the highest photosynthetic activity?",
+//   "options": [
+//       "Cotton",
+//       "Sugarcane",
+//       "Rice",
+//       "Wheat"
+//   ],
+//   "answer": "Sugarcane"
+//       },
+// "q16": {
+//   "question": "Which of the following crops has the highest photosynthetic activity?",
+//   "options": [
+//       "Cotton",
+//       "Sugarcane",
+//       "Rice",
+//       "Wheat"
+//   ],
+//   "answer": "Sugarcane"
+//       },
+// "q17": {
+//   "question": "Which of the following crops has the highest photosynthetic activity?",
+//   "options": [
+//       "Cotton",
+//       "Sugarcane",
+//       "Rice",
+//       "Wheat"
+//   ],
+//   "answer": "Sugarcane"
+//       },
+// "q18": {
+//   "question": "Which of the following crops has the highest photosynthetic activity?",
+//   "options": [
+//       "Cotton",
+//       "Sugarcane",
+//       "Rice",
+//       "Wheat"
+//   ],
+//   "answer": "Sugarcane"
+//       },
+// "q19": {
+//   "question": "Which of the following crops has the highest photosynthetic activity?",
+//   "options": [
+//       "Cotton",
+//       "Sugarcane",
+//       "Rice",
+//       "Wheat"
+//   ],
+//   "answer": "Sugarcane"
+//       },
+// "q20": {
+//   "question": "Which of the following crops has the highest photosynthetic activity?",
+//   "options": [
+//       "Cotton",
+//       "Sugarcane",
+//       "Rice",
+//       "Wheat"
+//   ],
+//   "answer": "Sugarcane"
+//       }
+};
+
+var score=0;
+var i=0;
+var j=0;
+
+function generateQuestion(){
+  document.getElementById('submitAns').style.display="inline";
+
+    i++;
+    //var k = Math.floor((Math.random() * 20) + 1);
+    k = "q"+i.toString();
+    console.log(k);
+    if(i<=10)
+    {
+      document.getElementById('question').innerHTML="<br><b>Question No."+(i)+"</b> <br><br> "+myObj[k].question;
+      document.getElementById('option1').innerHTML="<input type='radio' name='soption' id='1'> "+myObj[k].options[0];
+      document.getElementById('option2').innerHTML="<input type='radio' name='soption' id='2'> "+myObj[k].options[1];
+      document.getElementById('option3').innerHTML="<input type='radio' name='soption' id='3'> "+myObj[k].options[2];
+      document.getElementById('option4').innerHTML="<input type='radio' name='soption' id='4'> "+myObj[k].options[3];
+
+
+
+            console.log(myObj[k].answer);
+    }
+            if(i==11)
+            {
+              alert('Your score is:'+score);
+            }
+
+}
+function checkScore(){
+  document.getElementById('submitAns').style.display="none";
+  console.log('checkingScore');
+  if(document.getElementById("1").checked) {
+    if(document.getElementById("option1").innerText==" "+myObj[k].answer)
+    score++;
+  }else if(document.getElementById("2").checked) {
+      if(document.getElementById("option2").innerText==" "+myObj[k].answer)
+      score++;
+    }
+    else if(document.getElementById("3").checked) {
+        console.log('inside3');
+        if(document.getElementById("option3").innerText==" "+myObj[k].answer){
+          console.log('inside3.1');
+        score+=1;}
+      }
+      else if(document.getElementById("4").checked) {
+          if(document.getElementById("option4").innerText==" "+myObj[k].answer)
+          score++;
+        }
+
+else{
+
+}
+}
